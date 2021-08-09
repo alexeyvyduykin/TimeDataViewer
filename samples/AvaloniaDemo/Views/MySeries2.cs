707abf4d681e0800661e115dd846ccc187cb2de3
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2021-08-09 08:45:10 +0300

    Fix examples

--- samples/AvaloniaDemo/Views/MySeries2.cs
+++ samples/AvaloniaDemo/Views/MySeries2.cs
@@ -5,16 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using TimeDataViewer.ViewModels;
 using TimeDataViewer;
-using TimeDataViewer.Models;
 using AvaloniaDemo.ViewModels;
+using System.Collections;
 
 namespace AvaloniaDemo.Views
 {
     public class MySeries2 : Series
     {
-        public override IntervalTooltipViewModel CreateTooltip(IInterval marker)
+        public override TimeDataViewer.Core.Series CreateModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IntervalTooltipViewModel CreateTooltip(IntervalViewModel marker)
         {
             return new IntervalTooltipViewModel2(marker);
+        }
+
+        protected override void UpdateData(IEnumerable items)
+        {
+            throw new NotImplementedException();
         }
     }
 }
